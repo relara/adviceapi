5981648b5b544975a903b174f4e487c4410aa0ce
relara <relvira_7232@stu.southtexascollege.edu>
2022-03-08 19:20:45 -0600

    layout done

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-<h1>Home Page</h1>
+<div>
+    <input type="text" placeholder="Search and Advice..." class="w-full py-4 mt-16 text-xl text-center rounded-full" />
+</div>
+<div class="grid grid-cols-2 gap-16 pt-16">
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ullam dignissimos aspernatur quod minus explicabo, culpa sit adipisci nobis itaque eligendi debitis nisi ex dolorum rem velit quia? Nesciunt, assumenda?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ullam dignissimos aspernatur quod minus explicabo, culpa sit adipisci nobis itaque eligendi debitis nisi ex dolorum rem velit quia? Nesciunt, assumenda?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ullam dignissimos aspernatur quod minus explicabo, culpa sit adipisci nobis itaque eligendi debitis nisi ex dolorum rem velit quia? Nesciunt, assumenda?
+    </p>
+</div>
 </template>
